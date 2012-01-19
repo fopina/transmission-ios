c02db60701f6a846792ffd2e2027be9ef0923206
Filipe Pina <fopina@skmobi.com>
2012-01-19 14:07:35 +0000

    script to create deb & control file

---
+++ create_deb.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#########################
+ARCH="armv7"
+TRANSMISSION_VERSION=2.41
+#########################
+
+export BUILD_DIR="$PWD/out/${ARCH}/transmission"
+export TEMP_DIR="$PWD/deb"
+export DEBFILE="transmission-${TRANSMISSION_VERSION}-${ARCH}.deb"
+
+rm -fr ${TEMP_DIR}
+mkdir -p ${TEMP_DIR}
+
+mkdir -p ${TEMP_DIR}/DEBIAN
+cp control ${TEMP_DIR}/DEBIAN
+echo "Version: ${TRANSMISSION_VERSION}" >> ${TEMP_DIR}/DEBIAN/control
+cp -Rp ${BUILD_DIR} ${TEMP_DIR}/usr/
+dpkg-deb -b ${TEMP_DIR} ${DEBFILE}
