235de78bc5901cb0f3454835c804ed07ef296d3c
Filipe Pina <fopina@skmobi.com>
2012-01-20 11:24:37 +0000

    LaunchDaemon plist, postinst and prerm scripts

--- create_deb.sh
+++ create_deb.sh
@@ -7,10 +7,15 @@ export TEMP_DIR="$PWD/deb"
 export DEBFILE="transmission-${TRANSMISSION_VERSION}-${ARCH}.deb"
 
 rm -fr ${TEMP_DIR}
-mkdir -p ${TEMP_DIR}
 
 mkdir -p ${TEMP_DIR}/DEBIAN
-cp control ${TEMP_DIR}/DEBIAN
+cp deblayout/control deblayout/postinst deblayout/prerm ${TEMP_DIR}/DEBIAN
 echo "Version: ${TRANSMISSION_VERSION}" >> ${TEMP_DIR}/DEBIAN/control
+chmod 755 ${TEMP_DIR}/DEBIAN/postinst ${TEMP_DIR}/DEBIAN/prerm
+
+mkdir -p ${TEMP_DIR}/Library/LaunchDaemons
+cp deblayout/cc.fopina.transmission.plist ${TEMP_DIR}/Library/LaunchDaemons
+
 cp -Rp ${BUILD_DIR} ${TEMP_DIR}/usr
+
 dpkg-deb -b ${TEMP_DIR} ${DEBFILE}
